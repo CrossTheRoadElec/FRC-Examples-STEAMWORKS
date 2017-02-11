475f8dbaeda9f216072bc90c15678526e76c28b9
Omar Zrien <Ozrien@crosstheroadelectronics.com>
2017-02-11 14:59:40 -0500

    Added document comment.

--- JAVA_MotionMagicExample/src/org/usfirst/frc/team217/robot/Robot.java
+++ JAVA_MotionMagicExample/src/org/usfirst/frc/team217/robot/Robot.java
@@ -1,3 +1,21 @@
+/**
+ * Example demonstrating the motion magic control mode.
+ * Tested with Logitech F710 USB Gamepad inserted into Driver Station.
+ * 
+ * Be sure to select the correct feedback sensor using SetFeedbackDevice() below.
+ *
+ * After deploying/debugging this to your RIO, first use the left Y-stick 
+ * to throttle the Talon manually.  This will confirm your hardware setup/sensors
+ * and will allow you to take initial measurements.
+ * 
+ * Be sure to confirm that when the Talon is driving forward (green) the 
+ * position sensor is moving in a positive direction.  If this is not the 
+ * cause, flip the boolean input to the reverseSensor() call below.
+ *
+ * Once you've ensured your feedback device is in-phase with the motor,
+ * and followed the walk-through in the Talon SRX Software Reference Manual,
+ * use button1 to motion-magic servo to target position specified by the gamepad stick.
+ */
 package org.usfirst.frc.team217.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
